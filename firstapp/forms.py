8cf06711b95e7ef7b2eb4579fcfe6ca527c714d2
Dmitry <kiselevdv1991@gmail.com>
2020-11-30 16:26:03 +0300

    feat: add validators in validformapp

--- firstapp/forms.py
+++ firstapp/forms.py
@@ -5,7 +5,9 @@ from .models import Order, Pizza
 class OrderForm(forms.ModelForm):  # Форма от модели Order из models.py
     """Форма для заказа пиццы"""
     # Поле pizza - поле выбора (ModelChoiceField) всех пицц (Pizza.objects.all) в пиццерии
-    # HiddenInput - скрывает поле
+    # forms.ModelChoiceField соответсвует models.ForeignKey
+    # Для каждого типа поля в модели свой тип формы
+    # HiddenInput - виджет который скрывает поле в представлении браузера
     pizza = forms.ModelChoiceField(
         queryset=Pizza.objects.all(), widget=forms.HiddenInput)
 
--- validformapp/forms.py
+++ validformapp/forms.py
@@ -1,7 +1,34 @@
 from django import forms
+from django.core import validators
+
+
+# Кастомный валидатор на проверку value - данные из формы, хранятся в request.POST
+def check_for_a(value):
+    # value (вводимы текст должен начинаться с "N"
+    if value[0].upper() != 'N':
+        # Если (!= 'N') - вызов "raise" ошибки "ValidationError"
+        raise forms.ValidationError('Make needs to start with - "N"')  # Текст подсказка
 
 
 class NameForm(forms.Form):  # Форма от встроеного класса Form
-    name = forms.CharField()
+    name = forms.CharField(validators=[check_for_a])  # Кастомный валидатор выше
     email = forms.EmailField()
-    text = forms.CharField(widget=forms.Textarea)
+    # Повтор введённого email, "label" - название при отображении в HTML
+    verify_email = forms.EmailField(label="Enter your email again!")
+    # forms.Textarea - простой виджет отображения в HTML
+    # validators.MaxLengthValidator - встроенный валидатор количества введённых символов
+    text = forms.CharField(widget=forms.Textarea,
+                           validators=[validators.MaxLengthValidator(5)])
+
+    # Реализация метода form.clean - преобразует введенное в форму данные к нужному типу
+    # Заполняет преобразованными данными словарь form.cleaned_data и возвращает его
+    def clean(self):
+        # super() вызываем унаследованный от родителя "Form" метод "clean"
+        cleaned_data = super().clean()  # Получаем словарь с уже преобразованными данными
+        email = cleaned_data['email']  # Получаем данные из поля "email" в словаре cleaned_data
+        vmail = cleaned_data['verify_email']  # Тоже для verify_email
+
+        # Проверяем email на совпадение почты
+        if email != vmail:
+            raise forms.ValidationError('Make shure email match, email != vmail')
+        return cleaned_data
